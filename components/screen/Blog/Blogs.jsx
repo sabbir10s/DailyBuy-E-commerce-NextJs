596982087881673ef0ed752b8ddd5904c24467f4
sabbir10s <sabbirahmed1023@gmail.com>
2023-02-28 18:24:46 +0600

    recent blog added

--- components/screen/Blog/Blogs.jsx
+++ components/screen/Blog/Blogs.jsx
@@ -1,14 +1,29 @@
-import React from 'react';
-import { BlogsData } from '../../../StaticData/BlogsData';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
 import BlogCard from './BlogCard';
 
 const Blogs = () => {
-    
+  const [Blogs, setBlogs] = useState([])
+
+  const fetchData = () => {
+    axios
+      .get("/blogs.json")
+      .then((response) => {
+        setBlogs(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+  useEffect(() => {
+    fetchData();
+  }, []);
+
     return (
         <div className='container mt-[80px] lg:mt-[30px]'>
             <div className='grid md:grid-cols-2 lg:grid-cols-3 justify-center gap-[28px]'>
                 {
-                    BlogsData.map(blog=><BlogCard blog={blog} key={blog._id}/>)
+                    Blogs.map(blog=><BlogCard blog={blog} key={blog._id}/>)
                 }
             </div>
         </div>
--- components/screen/Blog/RecentBlog.jsx
+++ components/screen/Blog/RecentBlog.jsx
@@ -1,9 +1,26 @@
-import React from 'react';
-import { BlogsData } from '../../../StaticData/BlogsData';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
 import BlogCard from './BlogCard';
 
 const RecentBlog = ({ bid} ) => {
-    const recentBlog = BlogsData.filter(blog=>blog._id !==bid).slice(0,3)
+    
+  const [Blogs, setBlogs] = useState([])
+
+  const fetchData = () => {
+      axios
+          .get("/blogs.json")
+          .then((response) => {
+              setBlogs(response.data);
+          })
+          .catch((error) => {
+              console.log(error);
+          });
+  };
+  useEffect(() => {
+      fetchData();
+  }, [bid]);
+
+    const recentBlog = Blogs.filter(blog=>blog._id !==bid).slice(0,3)
     return (
         <div className='container'>
             <h2 className='text-2xl md:text-3xl lg:text-4xl font-bold text-center mt-[48px] lg:mt-[80px] mb-[48px]'>Related posts</h2>
