7dde44fe9ec8f8ab279ef3b78801f616a0b5f2c5
sabbir10s <sabbirahmed1023@gmail.com>
2023-02-28 16:28:03 +0600

    faq and aboutV2 page added

---
+++ pages/faq.jsx
@@ -0,0 +1,12 @@
+import FaqComponents from '@/components/screen/FaqComponents';
+import React from 'react';
+
+const faq = () => {
+    return (
+        <div>
+            <FaqComponents/>
+        </div>
+    );
+};
+
+export default faq;
