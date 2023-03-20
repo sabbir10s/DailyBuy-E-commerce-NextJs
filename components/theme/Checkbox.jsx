ffab668adf95cf987fc2eeb09f6bbe912982ebee
sabbir10s <sabbirahmed1023@gmail.com>
2023-03-20 22:35:49 +0600

    scroll style added

--- components/theme/Checkbox.jsx
+++ components/theme/Checkbox.jsx
@@ -4,7 +4,7 @@ import styles from "../../styles/checkbox.module.css";
 const Checkbox = () => {
   return (
     <>
-      <label className={`checkbox_btn ${styles.checkbox_btn}`}>
+      <label className={`agreeBox ${styles.agreeBox}`}>
         <label htmlFor="checkbox"></label>
         <input id="checkbox" type="checkbox" />
         <span className="checkmark"></span>
--- pages/_app.js
+++ pages/_app.js
@@ -4,20 +4,14 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import { Provider } from "react-redux";
 import { Wrapper } from "@googlemaps/react-wrapper";
-import { useRouter } from "next/router";
-import MenuBar from "@/components/shared/MenuBar";
 import MyNav from "@/components/shared/Nav/MyNav";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
 
 export default function App({ Component, pageProps }) {
-  const router = useRouter()
   return <Provider store={store}>
     <Wrapper apiKey={process.env.MAPS_API_KEY}>
-      {/* {
-        router.pathname !== "/" && <MenuBar />
-      } */}
 
       <MyNav />
       <Component {...pageProps} />
