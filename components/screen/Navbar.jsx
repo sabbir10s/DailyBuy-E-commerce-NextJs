7e8d3738b920f703e24613c206173dc07d4ba75c
sabbir10s <sabbirahmed1023@gmail.com>
2023-03-20 21:30:35 +0600

    active link added

--- components/screen/Navbar.jsx
+++ components/screen/Navbar.jsx
@@ -1,28 +1,31 @@
 import Link from "next/link";
+import { useRouter } from "next/router";
 import React from "react";
 
 
-const Navbar = (props) => {
+const Navbar = () => {
+  const router = useRouter()
   return (
     <nav className="hidden lg:block">
       <ul className="flex items-center gap-10 py-3 text-black">
         <li className="txtHover active font-medium">
-          <Link href="/">Home</Link>
+          <Link  className={router.pathname == "/" ? "text-primary-600" : "text-black"}
+    href="/">Home</Link>
         </li>
         <li className="txtHover active font-medium">
-          <Link href="/categories">Shop</Link>
+          <Link className={router.pathname == "/categories" ? "text-primary-600" : "text-black"} href="/categories">Shop</Link>
         </li>
         <li className="txtHover font-medium">
           {" "}
-          <Link href="/blog">Blog</Link>
+          <Link className={router.pathname == "/blog" ? "text-primary-600" : "text-black"} href="/blog">Blog</Link>
         </li>
         <li className="txtHover font-medium">
           {" "}
-          <Link href="/about">About us</Link>
+          <Link className={router.pathname == "/about" ? "text-primary-600" : "text-black"} href="/about">About us</Link>
         </li>
         <li className="txtHover font-medium">
           {" "}
-          <Link href="/contact">Contact us</Link>
+          <Link className={router.pathname == "/contact" ? "text-primary-600" : "text-black"} href="/contact">Contact us</Link>
         </li>
       </ul>
     </nav>
